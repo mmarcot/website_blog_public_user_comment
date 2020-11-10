137a5ae67349a00ce528b33c42c51dd397baf92d
Mallory MARCOT <mmarcot@gmail.com>
2020-11-10 12:25:05 +0100

    [IMP] displaying the public comment on the webpage

--- models/models.py
+++ models/models.py
@@ -10,4 +10,10 @@ class website_blog_public_user_comment(models.Model):
     name = fields.Char()
     email = fields.Char()
     content = fields.Text()
+    blog_post_id = fields.Many2one('blog.post', 'Blog post', ondelete='cascade', required=True)
 
+
+class BlogPost(models.Model):
+    _inherit = 'blog.post'
+
+    public_comment_ids = fields.One2many('website_blog_public_user_comment.comment', 'blog_post_id', 'Public comments')
