36bdb838f8363fc1fd661cdeede1653fd3b73495
Mallory MARCOT <mmarcot@gmail.com>
2020-11-12 11:22:20 +0100

    [REM] removing useless files

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,7 @@
     """,
 
     'author': "Mallory MARCOT",
-    'website': "http://www.mallory-marcot.com",
+    'website': "https://www.mallory-marcot.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -32,6 +32,5 @@
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
     ],
 }
